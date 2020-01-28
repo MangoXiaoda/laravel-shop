1d226e70f2e6858a5c48b859e41d11a6eb68e1ff
Mango <mangoxiaoda@gmail.com>
2020-01-28 05:35:28 +0000

    Alipay

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -19,6 +19,7 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        //
+        // 支付宝白名单
+        'payment/alipay/notify',
     ];
 }
--- routes/web.php
+++ routes/web.php
@@ -76,7 +76,19 @@ Route::group(['middleware' => ['auth', 'verified']], function(){
     Route::get('orders/{order}', 'OrdersController@show')
         ->name('orders.show');
 
+    // 支付宝支付
+    Route::get('payment/{order}/alipay', 'PaymentController@payByAlipay')
+        ->name('payment.alipay');
+
+    // 支付宝前端回调
+    Route::get('payment/alipay/return', 'PaymentController@alipayReturn')
+        ->name('payment.alipay.return');
+
 });
+
+// 支付宝服务端回调
+Route::post('payment/alipay/notify', 'PaymentController@alipayNotify')
+    ->name('payment.alipay.notify');
 
 Route::get('products/{product}', 'ProductsController@show')
     ->name('products.show');
