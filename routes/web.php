736c7bb8e2dca35b3cc5df8328e23b2164016026
Mango <mangoxiaoda@gmail.com>
2020-02-08 06:20:43 +0000

    检查优惠券

--- routes/web.php
+++ routes/web.php
@@ -104,6 +104,9 @@ Route::group(['middleware' => ['auth', 'verified']], function(){
     Route::post('orders/{order}/apply_refund', 'OrdersController@applyRefund')
         ->name('orders.apply_refund');
 
+    // 检查优惠券路由
+    Route::get('coupon_codes/{code}', 'CouponCodesController@show')
+        ->name('coupon_codes.show');
 });
 
 // 支付宝服务端回调
